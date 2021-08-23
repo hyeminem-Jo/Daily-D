ab0fd7957675af2af4b44a49a04711ea7d6b01de
hyeminem-Jo <konnimey@naver.com>
2021-08-23 21:07:22 +0900

    weather

--- js/weather.js
+++ js/weather.js
@@ -3,7 +3,7 @@ const API_KEY = "1c0df6c810c81d8611d1fc8b1549c05f" // API key 변수 넣기
 function onGeoOk(position) {
   const lat = position.coords.latitude
   const lon = position.coords.longitude
-  // 위의 값들을 geolocation 에서 얻은 값으로 바꿔보자
+  // 위의 값들을 geolocation 에서 얻은 값으로 바꾸기
   const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`
   fetch(url)
     .then(response => response.json())
@@ -19,6 +19,8 @@ function onGeoOk(position) {
 
 function onGeoError() {
   alert("Can't find you. No weather for you.")
+  const weatherId = document.getElementById("weather")
+  weatherId.remove()
 }
 
 navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError)
