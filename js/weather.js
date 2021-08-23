5ed6e9d981601eedd36b8ef3d7d0efd452630fec
hyeminem-Jo <konnimey@naver.com>
2021-08-23 20:08:37 +0900

    https

--- js/weather.js
+++ js/weather.js
@@ -4,7 +4,7 @@ function onGeoOk(position) {
   const lat = position.coords.latitude
   const lon = position.coords.longitude
   // 위의 값들을 geolocation 에서 얻은 값으로 바꿔보자
-  const url = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`
   fetch(url)
     .then(response => response.json())
     .then((data) => {
